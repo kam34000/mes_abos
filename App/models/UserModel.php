13504529fa9f33dc3bc887de06565a017377dc0b
John <john.bioux@gmail.com>
2019-07-26 16:31:54 +0200

    create user model

--- App/models/UserModel.php
+++ App/models/UserModel.php
@@ -1,16 +1,33 @@
 <?php
-namespace App\models;
-use Core\Models\Model;
+// crud
 
-class UserModel extends Model 
+namespace App\models;
+
+use Core\Models\Model;
+use \PDO;
+
+class UserModel extends Model
 {
-  public function create(array $data){
-      try {
-          if {
-              $req 
-          }
-      } catch (\Throwable $th) {
-          //throw $th;
-      }
-  }
+    public function create(array $datas)
+    {
+        try {
+            if (
+                ($req = $this->_db->prepare(
+                    'INSERT INTO `user`(`user_name`, `user_nickname`, `user_email`, `user_number`, `user_password`) VALUE (:name, :nickname, :email, :number, :password)'
+                )) !== false
+            ) {
+                if (
+                    $req->bindValue('name', $datas['name'])
+                    && $req->bindValue('nickname', $datas['firstname'])
+                    && $req->bindValue('email', $datas['email'])
+                    && $req->bindValue('number', $datas['phone'])
+                    && $req->bindValue('password', $datas['password'])
+                ) {
+                    if ($req->exectute()) { }
+                }
+            }
+        } catch (PDOException $e) {
+            throw new Exception($e->getMessage(), $e->getCode(), $e);
+        }
+    }
 }
