0ad8cd47079723da09850397db1f0ce05af63876
John <john.bioux@gmail.com>
2019-07-26 18:49:58 +0200

    verif user

--- App/Controller/RegisterController.php
+++ App/Controller/RegisterController.php
@@ -7,28 +7,39 @@ use App\models\UserModel;
 class RegisterController extends AppController
 {
 
-    public function index()
+    public function index(array $messages = [])
     {
+        $success = [];
+        $errors = [];
+        if (isset($messages['errors'])) {
+            $errors = $messages['errors'];
+        }
         $title = 'Home';
         $name = 'Karim';
-        $this->render('register/index', \compact('title', 'name'));
+        $this->render('register/index', \compact('title', 'name', 'success', 'errors'));
     }
     public function post()
     {
-        $title = 'Home';
 
-
-        if ($this->verifUser($_POST)) {
+        $errors = $this->verifUser($_POST);
+        if (count($errors) === 0) {
             $_POST['password'] = \password_hash($_POST['password'], PASSWORD_BCRYPT, ['cost' => 10]);
             $userModel = new UserModel();
             $userModel->create($_POST);
             $loginController = new LoginController();
             return $loginController->index(['success' => 'Votre compte a été crée, vous pouvez maintenant vous connecter.']);
+        } else {
+            $this->index(['errors' => $errors]);
         }
     }
 
-    private function verifUser(array $post)
+    private function verifUser(array $post): array
     {
-        return true;
+        $errors = [];
+        if (empty($_POST['email'])) {
+
+            return $errors['email'] = 'Ne dois pas être vide';
+        }
+        return $errors;
     }
 }
